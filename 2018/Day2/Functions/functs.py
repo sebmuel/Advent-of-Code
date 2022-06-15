b792e5eea72ef8d476020ba8e4ae5f152ab501b1
sebmuel <mueller.sebastian117@protonmail.com>
2022-06-15 19:37:36 +0200

    Added comments and reordered functions

--- 2018/Day2/Functions/functs.py
+++ 2018/Day2/Functions/functs.py
@@ -1,3 +1,7 @@
+"""
+PART 1
+"""
+
 def create_id_dictonary(_id: str) -> dict[int, str]:
     packet_ids = dict()
     for c in _id:
@@ -7,25 +11,6 @@ def create_id_dictonary(_id: str) -> dict[int, str]:
             packet_ids[c] = 1
     # switch keys and values with each other
     return {value: key for key, value in packet_ids.items()}
-
-
-def compare_ids(_id_one, _id_two):
-    match = 0
-    for i in range(len(_id_one)):
-        index_a = _id_one[i]
-        index_b = _id_two[i]
-        if index_a == index_b:
-            match += 1
-    return match
-
-
-def remove_diff(_id_one: str, _id_two: str):
-    final_id = ""
-    for char in _id_one:
-        if char not in _id_two:
-            final_id = _id_one.replace(char, "")
-            return final_id
-
 
 def part1(ids: [str]):
     pairs = 0
@@ -38,6 +23,26 @@ def part1(ids: [str]):
             triples += 1
     return pairs * triples
 
+
+"""
+PART 2
+"""
+
+def compare_ids(_id_one, _id_two):
+    match = 0
+    for i in range(len(_id_one)):
+        index_a = _id_one[i]
+        index_b = _id_two[i]
+        if index_a == index_b:
+            match += 1
+    return match
+
+def remove_diff(_id_one: str, _id_two: str):
+    final_id = ""
+    for char in _id_one:
+        if char not in _id_two:
+            final_id = _id_one.replace(char, "")
+            return final_id
 
 def part2(ids: [str]):
     for _id_one in ids:
